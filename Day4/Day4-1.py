7bcafe09168438b81fb53205cde22a5c14ccf05c
Stijn Grievink <83001732+stijngrie@users.noreply.github.com>
2023-12-05 15:24:35 +0000

    Test

--- Day4/Day4-1.py
+++ Day4/Day4-1.py
@@ -1,6 +1,20 @@
-file1 = open('Day4/Day4-Input.txt', 'r')
+file1 = open('Day4/Day4-InputT.txt', 'r')
 Lines = file1.readlines()
+
+answer = 0
 
 for line in Lines:
     winningNumbers = line.split(":")[1].split("|")[0].split(" ")
-    print(winningNumbers)
+    winningNumbers = list(filter(lambda x: x != '', winningNumbers))
+    scratchNumbers = line.split(":")[1].split("|")[0].split(" ")
+    scratchNumbers = list(filter(lambda x: x != '', scratchNumbers))
+    
+    winAmount = 0
+    for number in scratchNumbers:
+        if number in winningNumbers:
+            if winAmount == 0:
+                winAmount += 1
+            else:
+                winAmount *= 2
+    answer += winAmount
+print(answer)
